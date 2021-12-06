e388f6f3c893f29a81d48e40cfe944e946bd17eb
tupe12334 <ofek.gabay.he@gmail.com>
2021-12-07 00:40:27 +0200

    add email validation

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -1,7 +1,11 @@
 import { spawnSync, execSync } from "child_process";
+const gitConfigCommand = "git config --list";
 
 export async function getUserEmail(): Promise<any> {
-  return execSync("git config --list")
+  const email = execSync(gitConfigCommand)
     .toString()
     .match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi)![0];
+  if (!email) throw new Error(`Missing email from ${gitConfigCommand}`);
+
+  return email;
 }
